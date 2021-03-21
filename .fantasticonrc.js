f6ab0d48a7eb7af412d0b0230083cd6d597c7e74
Crybaka <kryptolone@protonmail.com>
2021-03-21 20:15:46 +0200

    Login page and layout (SASS, HTML)

---
+++ .fantasticonrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "ol-icons",
+  inputDir: 'assets/icons',
+  outputDir: 'static/fonts/ol-icons',
+  fontTypes: ['ttf', 'woff', 'woff2'],
+  assetTypes: [],
+  normalize: true,
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,18 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        devNotes
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+
+  <section class="auth container">
+
+    <img class="auth__logo" src="img/svg/olverica_logo.svg" alt="logo">
+    <form class="auth__form">
+      <div class="auth__input">Your email adress</div>
+      <div class="auth__input">Your password code</div>
+      <button class="auth__button" type="button">confirm</button>
+      <a class="auth__link">Forgot your password?</a>
+    </form>
+
+  </section>
+
+
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
 
-export default Vue.extend({})
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
