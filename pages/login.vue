b037667d1ddd14b9dba0d46201c9e3d3f3e56dbb
x4e554c4c <wanderingdwarf03@gmail.com>
2021-03-22 16:27:44 +0200

    added property decorator package, removed garbage files

---
+++ pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <section class="auth container">
+
+    <img class="auth__logo" src="img/svg/olverica_logo.svg" alt="logo">
+    <form class="auth__form">
+      <div class="auth__input">Your email adress</div>
+      <div class="auth__input">Your password code</div>
+      <button class="auth__button" type="button">confirm</button>
+      <a class="auth__link">Forgot your password?</a>
+    </form>
+
+  </section>
+
+
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue' 
+import {Component} from 'nuxt-property-decorator'
+
+@Component
+export default class LoginPage extends Vue{
+
+}
+</script>
+
+
