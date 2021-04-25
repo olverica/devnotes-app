962fd999aa41df0b0b2987fcba999dc24b128077
Crybaka <kryptolone@protonmail.com>
2021-04-25 14:35:29 +0300

    Viewer title & section (HTML,SASS)

--- pages/index.vue
+++ pages/index.vue
@@ -45,129 +45,157 @@
 
         <div class="viewer__area">
 
-          <div class="viewer__card viewer__card--developers">
-            <div class="viewer__card_inner">
+          <div class="viewer__section">
+            <div class="viewer__card viewer__card--developers">
+              <div class="viewer__card_inner">
 
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">developers</i>
-                <button class="viewer__card_button ol-icons">more</button>
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">developers</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>developers</h5>
+                  <!-- <i class="sticker sticker--created">created</i> -->
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
+            </div>
 
-              <div class="viewer__card_title">
-                <h5>developers</h5>
-                <!-- <i class="sticker sticker--created">created</i> -->
+            <div class="viewer__card viewer__card--customer">
+              <div class="viewer__card_inner">
+
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">customer</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>customer</h5>
+                  <!-- <i class="sticker sticker--created">created</i> -->
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
+            </div>
 
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
+            <div class="viewer__card viewer__card--archive">
+              <div class="viewer__card_inner">
+
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">archive</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>archive</h5>
+                  <i class="sticker sticker--counted">7</i>
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
+            </div>
 
+            <div class="viewer__card viewer__card--trash">
+              <div class="viewer__card_inner">
+
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">trash</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>trash</h5>
+                  <i class="sticker sticker--counted">3</i>
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
+              </div>
             </div>
           </div>
 
-          <div class="viewer__card viewer__card--customer">
-            <div class="viewer__card_inner">
 
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">customer</i>
-                <button class="viewer__card_button ol-icons">more</button>
+          <div class="viewer__section">
+            <h2 class="viewer__title viewer__title--tag">Title</h2>
+
+            <div class="viewer__card viewer__card--folder">
+              <div class="viewer__card_inner">
+
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">folder</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>folder</h5>
+                  <i class="sticker sticker--updated">updated</i>
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
-
-              <div class="viewer__card_title">
-                <h5>customer</h5>
-                <!-- <i class="sticker sticker--created">created</i> -->
-              </div>
-
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
-              </div>
-
             </div>
-          </div>
 
-          <div class="viewer__card viewer__card--archive">
-            <div class="viewer__card_inner">
+            <div class="viewer__card viewer__card--file">
+              <div class="viewer__card_inner">
 
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">archive</i>
-                <button class="viewer__card_button ol-icons">more</button>
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">file</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>file</h5>
+                  <i class="sticker sticker--created">created</i>
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
-
-              <div class="viewer__card_title">
-                <h5>archive</h5>
-                <i class="sticker sticker--counted">7</i>
-              </div>
-
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
-              </div>
-
             </div>
-          </div>
 
-          <div class="viewer__card viewer__card--trash">
-            <div class="viewer__card_inner">
+            <div class="viewer__card viewer__card--folder">
+              <div class="viewer__card_inner">
 
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">trash</i>
-                <button class="viewer__card_button ol-icons">more</button>
+                <div class="viewer__card_header">
+                  <i class="viewer__card_icon ol-icons">folder</i>
+                  <button class="viewer__card_button ol-icons">more</button>
+                </div>
+
+                <div class="viewer__card_title">
+                  <h5>folder</h5>
+                  <i class="sticker sticker--updated">updated</i>
+                </div>
+
+                <div class="viewer__card_footer">
+                  <span class="viewer__card_details">0 files</span>
+                  <time class="viewer__card_date">may 0 2021</time>
+                </div>
+
               </div>
-
-              <div class="viewer__card_title">
-                <h5>trash</h5>
-                <i class="sticker sticker--counted">3</i>
-              </div>
-
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="viewer__card viewer__card--file">
-            <div class="viewer__card_inner">
-
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">file</i>
-                <button class="viewer__card_button ol-icons">more</button>
-              </div>
-
-              <div class="viewer__card_title">
-                <h5>file</h5>
-                <i class="sticker sticker--created">created</i>
-              </div>
-
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="viewer__card viewer__card--folder">
-            <div class="viewer__card_inner">
-
-              <div class="viewer__card_header">
-                <i class="viewer__card_icon ol-icons">folder</i>
-                <button class="viewer__card_button ol-icons">more</button>
-              </div>
-
-              <div class="viewer__card_title">
-                <h5>folder</h5>
-                <i class="sticker sticker--updated">updated</i>
-              </div>
-
-              <div class="viewer__card_footer">
-                <span class="viewer__card_details">0 files</span>
-                <time class="viewer__card_date">may 0 2021</time>
-              </div>
-
             </div>
           </div>
           
