21d9dae2a9c97aa0b631f76ddc9f124765baf00d
Neideltern <kryptolone@protonmail.com>
2021-08-02 13:59:03 +0300

    Alerts sketch (SASS, HTML)

---
+++ pages/alert_tmp.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <div class="alert-area">
+
+    <div class="alert alert--cookies">
+      <h5 class="alert__title">Cookies 🍪</h5>
+      <p class="alert__text">We use cookies on our website</p>
+      <button class="alert__button">Got it</button>
+    </div>
+
+    <div class="alert alert--success">
+      <h5 class="alert__title">Success!</h5>
+      <p class="alert__text">We use cookies on our website</p>
+      <button class="alert__button">OK</button>
+    </div>
+
+    <div class="alert alert--error">
+      <h5 class="alert__title">Error!</h5>
+      <p class="alert__text">We use cookies on our website</p>
+      <button class="alert__button">OK</button>
+    </div>
+
+  </div>
+  
+
+</template>
